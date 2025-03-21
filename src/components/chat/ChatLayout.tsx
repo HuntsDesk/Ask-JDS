972ccc023e0ac0a6759249906238ac9359acdb91
HuntsDesk <hunter@talloak.com>
2025-03-21 12:10:19 -0400

    tablet fixes

--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -645,7 +645,9 @@ const ChatLayout = () => {
           isExpanded || isDesktop ? 'translate-x-0' : '-translate-x-full'
         } ${
           isDesktop ? 'relative' : 'fixed'
-        } ${sidebarWidth} h-full transition-all duration-300 ease-in-out z-50`}
+        } ${sidebarWidth} h-full max-h-screen sidebar-container transition-all duration-300 ease-in-out z-50 ${
+          isExpanded ? 'expanded' : 'collapsed'
+        }`}
       >
         <Sidebar
           setActiveTab={handleSetActiveThread}
