294205c2205412854cea9630f0764cbd5f9bb2a6
HuntsDesk <hunter@talloak.com>
2025-03-21 12:26:22 -0400

    rollback 1

--- src/components/flashcards/FlashcardCollections.tsx
+++ src/components/flashcards/FlashcardCollections.tsx
@@ -715,7 +715,7 @@ export default function FlashcardCollections() {
   const selectedSubject = subjects.find(s => s.id === selectedSubjectId);
 
   return (
-    <div className="px-4 md:px-6 py-6 content-below-navbar">
+    <div className="w-full max-w-6xl mx-auto px-4 py-6">
       <DeleteConfirmation
         isOpen={!!collectionToDelete}
         onClose={() => setCollectionToDelete(null)}
